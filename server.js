d4dabd5f6f00ab4e6a14a1c3f67678e43d2b1d56
rayaraujoc93 <rayaraujoc93@gmail.com>
2025-12-11 17:41:27 -0300

    Update server.js

--- server.js
+++ server.js
@@ -53,5 +53,9 @@ app.get("/", (req, res) => {
 });
 
 // Porta
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => console.log("Servidor rodando na porta", PORT));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
